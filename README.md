be410a8c65d62a8de97d9fee832c0ef3fa98fcc0
Matt Edwards <mttdwrds@gmail.com>
2012-12-10 02:23:56 -0500

    Code and usability improvements.

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-multipool
-=========
+Multipool 0.10
+==============
 
-See the [wiki page](https://github.com/matted/multipool/wiki) for more details.
+See the [wiki page](https://github.com/matted/multipool/wiki) for more
+details, including usage examples and installation instructions.
+
+
+usage: mp_inference.py [-h] -n N [-m {replicates,contrast}] [-r RES] [-c CM]
+                       [-t FILTER] [-np] [-o OUTFILE] [-v]
+                       countfile [countfile ...]
+
+Multipool: Efficient multi-locus genetic mapping with pooled sequencing,
+version 0.10. See http://cgs.csail.mit.edu/multipool/ for more details.
+
+positional arguments:
+  countfile             Input file[s] of allele counts
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n N, --individuals N
+                        Individuals in each pool (required)
+  -m {replicates,contrast}, --mode {replicates,contrast}
+                        Mode for statistical testing. Default: replicates
+  -r RES, --resolution RES
+                        Bin size for discrete model. Default: 100 bp
+  -c CM, --centimorgan CM
+                        Length of a centimorgan, in base pairs. Default: 3300
+                        (yeast average)
+  -t FILTER, --truncate FILTER
+                        Truncate possibly fixated (erroneous) markers.
+                        Default: true
+  -np, --noPlot         Turn off plotting output.. Default: false
+  -o OUTFILE, --output OUTFILE
+                        Output file for bin-level statistics
+  -v, --version         show program's version number and exit
+
+
+Count file format: 
+
+A whitespace delimited file with a row for each marker (SNP or small
+indel).  The first column reports the locus position in base pairs
+(used with the --centimorgan parameter to compute crossover
+probabilities).  The second column reports the number of sequencing
+reads from the first analyzed strain and the third column reports the
+read count from the second strain.
--- mp_inference.py
+++ mp_inference.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
-## TODO:
-## - the plot is plotting the null model average, not the best fit one.
-## is MLE distribution of allele frequencies shifted over off the peak for small bin sizes?
-## - finish wiki pages.
-## - test distribution/usability.
 
-import argparse, collections, sys
-import matplotlib, numpy, pylab, scipy.stats
+import argparse, collections, sys, logging
+import numpy, scipy.stats
+
+VERSION = "0.10"
 
 def load_table(fin, binsize, verbose, filt):
     temp = collections.defaultdict(lambda : numpy.zeros(2))
@@ -14,14 +11,16 @@ def load_table(fin, binsize, verbose, filt):
         if line.startswith("#"): continue
         line = line.strip().split()
         line = map(float, line[:3])
-        # if len(line) <= 1: continue
         a, b = line[1:3]
-        if filt and (a <= 0 or b <= 0): # we might miss really informative SNPs, but we're probably just missing fixated markers... so skip them
+        if filt and (a <= 0 or b <= 0): # We might miss really
+                                        # informative SNPs, but we're
+                                        # probably just missing
+                                        # fixated markers... so skip
+                                        # them
             if verbose and a+b>0: print >>sys.stderr, "Skipping", line
             continue
         temp[line[0] / binsize] += (a,b)
     fin.close()
-
 
     if filt:
         # Filter highly-outlying counts.  Preprocessing should take
@@ -41,7 +40,7 @@ def load_table(fin, binsize, verbose, filt):
     counts = numpy.zeros(max(temp.keys())+1)
     variances = numpy.zeros(max(temp.keys())+1) + float("inf")
     for loc, (a,b) in temp.iteritems():
-        p = 1.0*a/(a+b)
+        p = 1.0*a/(a+b+1e-6)
         means[loc] = a
         counts[loc] = a+b
         if a+b > 0:
@@ -54,10 +53,10 @@ def load_table(fin, binsize, verbose, filt):
 def lognormpdf(x, mu, sigma):
     return -0.5*numpy.log(2*numpy.pi) - numpy.log(sigma) + (-(x-mu)**2.0/2.0/sigma**2.0)
 
-# this calculates posterior estimates of the means and variances
-# at each point in the sequence
+# Use the Kalman filtering equations to calculate posterior estimates
+# of the means and variances at each point in the sequence
 # 
-# so: P(x_i | y) = N(x_i | mu_pstr, V_pstr)
+# So we'll give: P(x_i | y) = N(x_i | mu_pstr, V_pstr)
 #
 def kalman(y, y_var, d, T, N, p):
     mu = numpy.zeros(T)
@@ -69,8 +68,9 @@ def kalman(y, y_var, d, T, N, p):
 
     c = numpy.ones(T)
 
-    mu_initial = 0.5*N # initial parameters, assumed given
-    V_initial = 0.25*N # ditto
+    mu_initial = 0.5*N # Initial parameters, assumed given (binomial
+                       # distribution)
+    V_initial = 0.25*N
 
     A = (1.0 - 2.0*p)
     C = 1.0 * d / N
@@ -85,14 +85,14 @@ def kalman(y, y_var, d, T, N, p):
     else:
         c[0] = 1.0
 
-    # Forward pass
+    # Forward pass:
     for i in xrange(1,T):
-        # P[i] = A**2.0 * V[i] + S # how did this work?
         if i == 1:
             P[i-1] = A**2.0*V_initial + S
         else:
             P[i-1] = A**2.0*V[i-1] + S
-        if y_var[i] == float("inf"): # no observation here
+        if y_var[i] == float("inf"): # No observation here: infinite
+                                     # uncertainty.
             K = 0
             c[i] = 1.0
         else:
@@ -107,7 +107,7 @@ def kalman(y, y_var, d, T, N, p):
 
     logLik = numpy.sum(numpy.log(c))
 
-    # backward pass
+    # Backwards pass:
     for i in xrange(T-2,-1,-1):
         J = V[i]*A/P[i]
         mu_pstr[i] = mu[i] + J * (mu_pstr[i+1] - A*(mu[i]) - N*p)
@@ -161,53 +161,17 @@ def calcLODs_multicoupled(mu_pstr_vec, V_pstr_vec, T, N):
         # LOD[i] = numpy.log10(N*(x[1]-x[0]) * allsums.max())
         LOD[i] = numpy.log10(N) + numpy.log10(x[1]-x[0]) + logallsums.max() / numpy.log(10.0)
         
+        # A few sanity checks for development:
         # assert(LOD[i] > -1e-6)
         # assert(LOD[i] == LOD[i]) # check for nan
         # assert(LOD[i] != LOD[i]+1) # check for +/- inf
 
     return LOD, mu_MLE
 
-def doOutput():
-    pass
-
-def parseArgs():
-    parser = argparse.ArgumentParser(description="Multipool description")
-
-    parser.add_argument("-f", "--countfile[s]", type=argparse.FileType("r"), nargs=argparse.REMAINDER, help="Input file[s], space separated for multiple files.", required=True, dest="fins")
-    parser.add_argument("-n", "--individuals", type=int, help="Individuals in each pool", required=True, dest="N")
-    parser.add_argument("-m", "--mode", choices=["replicates", "contrast", "combine"], default="replicates", help="Mode for statistical testing.  Default: replicates.", dest="mode")
-    parser.add_argument("-r", "--resolution", type=float, default=100, help="Bin size for discrete model.  Default: 100 bp.", dest="res")
-    parser.add_argument("-c", "--centimorgan", type=float, default=3300, help="Length of a centimorgan, in base pairs.  Default: 3300 (yeast average).", dest="cM")
-    parser.add_argument("-t", "--truncate", type=bool, default=True, help="Truncate possibly fixated (erroneous) markers.  Default: true.", dest="filter")
-
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.8")
-
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    args = parseArgs()
-
-    print >>sys.stderr, "Multipool version:", "TODO"
-    print >>sys.stderr, "Python version:", sys.version
-    print >>sys.stderr, "Scipy version:", scipy.__version__
-    print >>sys.stderr, "Numpy version:", numpy.__version__
-    print >>sys.stderr, "Matplotlib version:", matplotlib.__version__
-    
-    N = args.N
-    res = args.res
-    p = res/100.0/args.cM # was 3000.0
-    fins = args.fins
-    filt = args.filter
-
-    REPLICATES = (args.mode == "replicates")
-    COMBINE_DATA = (args.mode == "combine")
-
-    SIMPLE = False
-    MOREINFO = True # what's this?
-    
-    print >>sys.stderr, "Recombination fraction:", p, "in cM:", 1.0*res/p/100.0
-
+def doLoading(fins, filt):
     y,y_var,d = load_table(fins[0], res, False, filt)
+    d2 = None
+    y_var2 = None
 
     if len(fins) > 1:
         y2, y_var2, d2 = [], [], []
@@ -222,7 +186,7 @@ if __name__ == "__main__":
     print >>sys.stderr, "Loaded %d informative reads" % sum(d)
 
     if y2 is None:
-        T = len(y) # observations (max time)
+        T = len(y) # Observations (max time index)
     else:
         T = min([len(temp) for temp in y2])
         y = y[:T]
@@ -234,12 +198,6 @@ if __name__ == "__main__":
             y_var2[i] = y_var2[i][:T]
             d2[i] = d2[i][:T]
 
-        if COMBINE_DATA: # this doesn't work any more (y2, etc. are now lists)
-            d += d2
-            y = (y+y2)
-            y_var = (y_var + y_var2)
-            y2 = None
-
     start, stop = 0,0 # T/2,T # 2*T/5, T # 2*T/5, T # 2*T/11, T
     y_var[start:stop] = float("inf")
     if y2 is not None:
@@ -250,46 +208,45 @@ if __name__ == "__main__":
         for i in xrange(len(d2)):
             d2[i][start:stop] = 0
 
+    return y, y_var, y2, y_var2, d, d2, T
+
+def doOutput():
+    pass
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Multipool: Efficient multi-locus genetic mapping with pooled sequencing, version %s.  See http://cgs.csail.mit.edu/multipool/ for more details." % VERSION)
+
+    parser.add_argument("fins", metavar="countfile", type=argparse.FileType("r"), nargs="+", help="Input file[s] of allele counts")
+    parser.add_argument("-n", "--individuals", type=int, help="Individuals in each pool (required)", required=True, dest="N")
+    parser.add_argument("-m", "--mode", choices=["replicates", "contrast"], default="replicates", help="Mode for statistical testing.  Default: replicates", dest="mode")
+    parser.add_argument("-r", "--resolution", type=float, default=100, help="Bin size for discrete model.  Default: 100 bp", dest="res")
+    parser.add_argument("-c", "--centimorgan", type=float, default=3300, help="Length of a centimorgan, in base pairs.  Default: 3300 (yeast average)", dest="cM")
+    parser.add_argument("-t", "--truncate", type=bool, default=True, help="Truncate possibly fixated (erroneous) markers.  Default: true", dest="filter")
+    parser.add_argument("-np", "--noPlot", action="store_true", default=False, help="Turn off plotting output..  Default: false", dest="noPlot")
+
+    parser.add_argument("-o", "--output", type=argparse.FileType("w"), default=None, help="Output file for bin-level statistics", dest="outFile")
+
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s " + VERSION)
+
+    return parser.parse_args()
+
+def doPlotting(y, y2, d, d2, LOD, mu_MLE, mu_pstr, mu_pstr2, V_pstr, V_pstr2, left, right):
+    import pylab
     X = numpy.arange(0, T*res, res)
 
-    mu_pstr, V_pstr, logLik = kalman(y, y_var, d, T, N, p)
     if y2 is not None:
-        mu_pstr2 = []
-        V_pstr2 = []
         old = numpy.seterr(all="ignore")
-        for curr_y2, curr_y_var2, curr_d2 in zip(y2, y_var2, d2):
-            curr_mu_pstr2, curr_V_pstr2, ignored = kalman(curr_y2, curr_y_var2, curr_d2, T, N, p)
+        for curr_y2, curr_d2, curr_mu_pstr2, in zip(y2, d2, mu_pstr2):
             pylab.plot(X, curr_y2/curr_d2 , "+", alpha=0.6)
-            pylab.plot(X, curr_mu_pstr2/N, 'r', lw=2)
-            mu_pstr2.append(curr_mu_pstr2)
-            V_pstr2.append(curr_V_pstr2)
+            pylab.plot(X, curr_mu_pstr2/N, lw=2)
         numpy.seterr(**old)
 
-    LOD, mu_MLE = calcLODs_multicoupled([mu_pstr], [V_pstr], T, N)
-
-    if y2 is not None:
-        LOD3, mu_MLE3 = calcLODs_multicoupled(mu_pstr2, V_pstr2, T, N)
-
-        if REPLICATES:
-            LOD = LOD3
-            mu_MLE = mu_MLE3
-        else:
-            LOD2, mu_MLE2 = calcLODs_multicoupled([mu_pstr2[1]], [V_pstr2[1]], T, N)
-            assert(len(mu_pstr2) == 2)
-            LOD = LOD + LOD2 - LOD3
-
-    if False and not SIMPLE:
-        pylab.subplot(211)
-
-    numpy.seterr(divide="ignore")
+    old = numpy.seterr(all="ignore")
     pylab.plot(X, y/d, "r+", alpha=0.6)
-    numpy.seterr(divide="warn")
+    numpy.seterr(**old)
 
     pylab.xlabel("bp (%d bp loci)" % res)
     pylab.ylabel("Allele frequency")
-
-    # pylab.plot(X, mu_pstr/N, 'r', lw=2)
-    # pylab.plot(X, mu_MLE/N, 'r', alpha=0.5, lw=2)
 
     if y2 is not None:
         for val, alpha in [(0.025, 0.3), (0.005, 0.1), (0.0005, 0.05)]:
@@ -300,22 +257,65 @@ if __name__ == "__main__":
     else:
         for val, alpha in [(0.025, 0.3), (0.005, 0.1), (0.0005, 0.05)]:
             CI = scipy.stats.norm.isf(val, 0, numpy.sqrt(V_pstr))
-            pylab.fill_between(X, (mu_MLE - CI)/N, (mu_MLE + CI)/N, color='r', alpha=alpha)
-            # pylab.fill_between(X, (mu_pstr - CI)/N, (mu_pstr + CI)/N, color='r', alpha=alpha)
+            # pylab.fill_between(X, (mu_MLE - CI)/N, (mu_MLE + CI)/N, color='r', alpha=alpha)
+            pylab.fill_between(X, (mu_pstr - CI)/N, (mu_pstr + CI)/N, color='r', alpha=alpha)
 
-
-    # CI = scipy.stats.norm.isf(0.025, 0, numpy.sqrt(V))
-    # pylab.fill_between(numpy.arange(T), mu - CI, mu + CI, color='y', alpha=0.3)
-    # pylab.plot(V, '--')
-    # pylab.plot(V_pstr, '--', lw=2)
     pylab.axhline(0.5, color='k', ls=':')
+              
+    targStart = left*res
+    targStop = right*res
+    pylab.fill_between([targStart-res/2,targStop-res/2], 0, 1, color="k", alpha=0.2)
+
+    pylab.axis([0,T*res,0,1])
+
+    pylab.twinx()
+    pylab.ylabel("LOD score")
+    pylab.plot(X, LOD, 'g-', lw=2)
+
+    if N < 10000:
+        posteriors = numpy.zeros((N,T))
+        for c in xrange(T):
+            posteriors[:,c] = scipy.stats.norm.pdf(numpy.arange(0,1.0,1.0/N), mu_pstr[c]/N, numpy.sqrt(V_pstr[c])/N)
+            posteriors[:,c] /= numpy.sum(posteriors[:,c])
+
+    pylab.axis([0,T*res,LOD.min(),LOD.max()+3])
+    pylab.show()
+
+def doComputation(y, y_var, y2, y_var2, d, d2, T):
+    mu_pstr, V_pstr, logLik = kalman(y, y_var, d, T, N, p)
+
+    LOD, mu_MLE = calcLODs_multicoupled([mu_pstr], [V_pstr], T, N)
+    
+    mu_pstr2, V_pstr2 = None, None
+
+    if y2 is not None:
+        mu_pstr2 = []
+        V_pstr2 = []
+        old = numpy.seterr(all="ignore")
+        for curr_y2, curr_y_var2, curr_d2 in zip(y2, y_var2, d2):
+            curr_mu_pstr2, curr_V_pstr2, ignored = kalman(curr_y2, curr_y_var2, curr_d2, T, N, p)
+            mu_pstr2.append(curr_mu_pstr2)
+            V_pstr2.append(curr_V_pstr2)
+        numpy.seterr(**old)
+
+    if y2 is not None:
+        LOD3, mu_MLE3 = calcLODs_multicoupled([mu_pstr] + mu_pstr2, [V_pstr] + V_pstr2, T, N)
+
+        if REPLICATES:
+            LOD = LOD3
+            mu_MLE = mu_MLE3
+        else:
+            LOD2, mu_MLE2 = calcLODs_multicoupled(mu_pstr2, V_pstr2, T, N)
+            LOD = LOD + LOD2 - LOD3
 
     temp = numpy.exp(LOD) / numpy.sum(numpy.exp(LOD))
 
+    # Credible interval calculations need to unified and refactored to
+    # a common function:
     left = temp.argmax()
     right = left
     cumul = temp[left]
-    while cumul < 0.50: # and (left >= 0 and right < T):
+    while cumul < 0.50 and (left >= 0 and right < T):
         if temp[left] >= temp[right] and left > 0 or right == T-1 and left > 0:
             left -= 1
             cumul += temp[left]
@@ -325,11 +325,7 @@ if __name__ == "__main__":
         else:
             break
 
-    print >>sys.stderr, "Other 50% credible interval spans", left*res, right*res, "length is:", (right-left)*res
-              
-    targStart = left*res
-    targStop = right*res
-    pylab.fill_between([targStart-res/2,targStop-res/2], 0, 1, color="k", alpha=0.2)
+    print >>sys.stderr, "50% credible interval spans", left*res, right*res, "length is:", (right-left)*res
 
     cumul, mean = 0.0, 0.0
     left, right = None, None
@@ -344,51 +340,60 @@ if __name__ == "__main__":
     if right is None: right = T
 
     print >>sys.stderr, "90% credible interval spans", left*res, right*res, "length is:", (right-left)*res, "mean:", mean, "mode:", temp.argmax()*res
+    left90 = left
+    right90 = right
 
-    pylab.axis([0,T*res,0,1])
-    # pylab.axis([0,400000,0,1])
-    if not SIMPLE:
-        pylab.twinx()
-        pylab.ylabel("LOD score")
-        pylab.plot(X, LOD, 'g-', lw=2)
+    maxLOD = LOD.max()
+    maxIndex = LOD.argmax()
+    print >>sys.stderr, "Max multi-locus LOD score at:", maxLOD, maxIndex*res
+    index = maxIndex
+    while index >= 0 and LOD[index] > maxLOD-1.0:
+        index -= 1
+    left = index
+    print >>sys.stderr, "1-LOD interval from ", index*res,
+    index = maxIndex
+    while index < T and LOD[index] > maxLOD-1.0:
+        index += 1
+    print >>sys.stderr, "to", index*res, "length is:", (index-left)*res
 
-    if not SIMPLE:
-        if False and MOREINFO: pylab.plot(X, temp / numpy.max(temp) * numpy.max(LOD), 'g')
-        # pylab.plot(X, scipy.stats.norm.pdf(X, mean, stdev) / scipy.stats.norm.pdf(mean, mean, stdev) * numpy.max(LOD), 'g:')
-    # print LOD
+    D = 30 # Assume that contributions to the location are effectively
+           # zero when you go this many bins away.
+    try:
+        print >>sys.stderr, "Sublocalized best location:", numpy.sum(numpy.arange(res*(maxIndex-D),res*(maxIndex+D+1),res)*numpy.exp(LOD[maxIndex-D:maxIndex+D+1])) / numpy.sum(numpy.exp(LOD[maxIndex-D:maxIndex+D+1]))
+    except ValueError:
+        pass
 
+    return LOD, mu_MLE, mu_pstr, mu_pstr2, V_pstr, V_pstr2, left90, right90
+    
+if __name__ == "__main__":
+    args = parseArgs()
 
-    if False and not SIMPLE:
-        pylab.subplot(212)
-    if N < 10000:
-        posteriors = numpy.zeros((N,T))
-        for c in xrange(T):
-            posteriors[:,c] = scipy.stats.norm.pdf(numpy.arange(0,1.0,1.0/N), mu_pstr[c]/N, numpy.sqrt(V_pstr[c])/N)
-            posteriors[:,c] /= numpy.sum(posteriors[:,c])
-        if False and not SIMPLE:
-            pylab.imshow((numpy.log10(posteriors)), interpolation="nearest", origin="lower", cmap=pylab.get_cmap("YlGnBu"))
+    print >>sys.stderr, "Multipool version:", VERSION
+    print >>sys.stderr, "Python version:", sys.version
+    print >>sys.stderr, "Scipy version:", scipy.__version__
+    print >>sys.stderr, "Numpy version:", numpy.__version__
+    if not args.noPlot:
+        import matplotlib
+        print >>sys.stderr, "Matplotlib version:", matplotlib.__version__
 
-    if True:
-        maxLOD = LOD.max()
-        maxIndex = LOD.argmax()
-        print >>sys.stderr, "max multi-locus LOD score at:", maxLOD, maxIndex*res
-        index = maxIndex
-        while index >= 0 and LOD[index] > maxLOD-1.0:
-            index -= 1
-        left = index
-        print >>sys.stderr, "1-LOD interval from ", index*res,
-        index = maxIndex
-        while index < T and LOD[index] > maxLOD-1.0:
-            index += 1
-        print >>sys.stderr, "to", index*res, "length is:", (index-left)*res
+    N = args.N
+    res = args.res
+    p = res/100.0/args.cM
 
-        # print "p_alternate at highest LOD score segment:", (means[maxIndex] + 1.0) / (N+2.0)
-        D = 30
-        try:
-            print >>sys.stderr, "sublocalized best location:", numpy.sum(numpy.arange(res*(maxIndex-D),res*(maxIndex+D+1),res)*numpy.exp(LOD[maxIndex-D:maxIndex+D+1])) / numpy.sum(numpy.exp(LOD[maxIndex-D:maxIndex+D+1]))
-        except ValueError:
-            pass
+    REPLICATES = (args.mode == "replicates")
 
-    pylab.axis([0,T*res,LOD.min(),LOD.max()+3])
-    # pylab.axis([0,400000,0,LOD.max()+3])
-    pylab.show()
+    print >>sys.stderr, "Recombination fraction:", p, "in cM:", 1.0*res/p/100.0
+
+    # Data loading and preprocessing.
+    y, y_var, y2, y_var2, d, d2, T = doLoading(args.fins, args.filter)
+
+    # Main computation.
+    LOD, mu_MLE, mu_pstr, mu_pstr2, V_pstr, V_pstr2, left, right = doComputation(y, y_var, y2, y_var2, d, d2, T)
+
+    # Do something with the results.
+    if args.outFile is not None:
+        doOutput(args.outFile)
+
+    if not args.noPlot:
+        doPlotting(y, y2, d, d2, LOD, mu_MLE, mu_pstr, mu_pstr2, V_pstr, V_pstr2, left, right)
+
