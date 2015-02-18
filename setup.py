20eb628919e7d3f841c2934fa62acc395d745040
Matt Edwards <mttdwrds@gmail.com>
2015-02-18 09:42:52 -0500

    Added Travis config and a basic setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name="multipool",
+      version="0.10.1",
+      description="Efficient multi-locus genetic mapping with pooled sequencing.",
+      author="Matt Edwards",
+      author_email="matted@mit.edu",
+      license="MIT",
+      url="https://github.com/matted/multipool",
+      packages=[],
+      scripts=["mp_inference.y"],
+      zip_safe=True,
+      install_requires=["scipy", "numpy"], # pylab is optional; leaving it out for now
+  )
